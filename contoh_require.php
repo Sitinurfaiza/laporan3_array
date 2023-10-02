69ad726f32bc100937340c354f427382915134f9
Sitinurfaiza <112322032+Sitinurfaiza@users.noreply.github.com>
2023-10-02 21:09:17 +0700

    Add files via upload

---
+++ contoh_require.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>siti_nurfaiza</title>
+</head>
+<body>
+    <?php
+    $a = "Saya sedang belajar PHP";
+    function tulistebal($teks)
+    {
+        echo "<b>$teks</b>";
+    }
+    
+    tulistebal($a);
+    ?>
+</body>
+</html>
---
+++ panggil_require.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Siti_nurfaiza</title>
+</head>
+<body>
+    <?php
+    require("contoh_require.php");
+    echo "<br>";
+    tulistebal("Ini adalah tulisan tebal");
+    echo "<br>";
+    echo $a;
+?>
+</body>
+</html>
